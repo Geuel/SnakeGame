72ec270e86652944a6d8c7c5a8e4c6b281fdc9f2
Geuel Chaves Paiva <geuelcp@gmail.com>
2021-07-05 20:50:41 -0300

    Snake move (up, down, left, right)

--- script.js
+++ script.js
@@ -20,7 +20,18 @@ function criarCobrinha() {
   }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event) {
+  if(event.keyCode == 37 && direction != "right") direction = "left";
+  if(event.keyCode == 38 && direction != "down") direction = "up";
+  if(event.keyCode == 39 && direction != "left") direction = "right";
+  if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo() {
+  
+
   criarBG();
   criarCobrinha();
 
@@ -42,4 +53,4 @@ function iniciarJogo() {
   snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 1000);
